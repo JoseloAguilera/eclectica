11f589e9c4c273672b367329031fe987363c882b
Yanina Escobar <78667442+LuzEscobar@users.noreply.github.com>
2022-12-01 15:28:46 -0300

    .

--- vistas/modal/registro_cliente.php
+++ vistas/modal/registro_cliente.php
@@ -11,7 +11,152 @@ if (isset($conexion)) {
 				<div class="modal-body">
 					<form class="form-horizontal" method="post" id="guardar_cliente" name="guardar_cliente">
 						<div id="resultados_ajax"></div>
-						<div class="row">
+						<ul class="nav nav-tabs">
+							<li class="nav-item">
+								<a href="#empresa" data-toggle="tab" aria-expanded="false" class="nav-link active">
+									Datos Personales
+								</a>
+							</li>
+							<li class="nav-item">
+								<a href="#contacto" data-toggle="tab" aria-expanded="true" class="nav-link">
+									Datos de Contacto
+								</a>
+							</li>
+						</ul>
+						<div class="tab-content">
+							<div class="tab-pane fade show active" id="empresa">
+
+								<div class="row">
+									<div class="col-md-12">
+										<div class="form-group">
+											<label for="nombre_cliente" class="control-label">Razón Social:</label>
+											<input type="text" class="form-control UpperCase" id="nombre" name="nombre_cliente" autocomplete="off" required>
+										</div>
+									</div>
+								</div>
+								<div class="row">
+									<div class="col-md-6">
+										<div class="form-group">
+											<label for="fiscal_cliente" class="control-label">RUC:</label>
+											<input type="text" class="form-control" id="fiscal" name="fiscal_cliente" autocomplete="off" required>
+										</div>
+									</div>
+									<div class="col-md-6">
+										<div class="form-group">
+											<label for="telefono_cliente" class="control-label">Telefono:</label>
+											<input type="text" class="form-control" id="telefono_cliente" name="telefono_cliente" autocomplete="off">
+										</div>
+									</div>
+								</div>
+								
+								<div class="row">
+									<div class="col-md-12">
+										<div class="form-group">
+											<label for="direccion_cliente" class="control-label">Dirección:</label>
+											<textarea class="form-control UpperCase"  id="direccion_cliente" name="direccion_cliente" maxlength="255" autocomplete="off"></textarea>
+										</div>
+									</div>
+								</div>
+								<div class="row">
+								<div class="col-md-6">
+										<div class="form-group">
+											<label for="email_cliente" class="control-label">Email:</label>
+											<input type="email_cliente" class="form-control" id="email_cliente" name="email_cliente" autocomplete="off">
+										</div>
+									</div>
+									<div class="col-md-6">
+										<div class="form-group">
+											<label for="status_cliente" class="control-label">Estado:</label>
+											<select class="form-control" id="status_cliente" name="status_cliente" required>
+												<option value="">-- Selecciona --</option>
+												<option value="1" selected>Activo</option>
+												<option value="0">Inactivo</option>
+											</select>
+										</div>
+									</div>
+									
+								</div>
+
+								<div class="row">
+									<div class="col-md-12">
+										<div class="form-group">
+											<label for="fec_nacimiento" class="control-label">Nacimiento</label>
+											<input type="date" class="form-control UpperCase" id="fec_nacimiento" name="fec_nacimiento" autocomplete="off" required>
+										</div>
+									</div>
+								</div>
+
+								<div class="row">
+									
+									<div class="col-md-6">
+										<div class="form-group">
+											<label for="ciudad" class="control-label">Ciudad:</label>
+											<select class="form-control" id="ciudad" name="ciudad" required>
+												<option value="">-- Selecciona --</option>
+												<option value="CDE" selected>CDE</option>
+												<option value="ASUNCION">Asuncion</option>
+											</select>
+										</div>
+									</div>
+									
+								</div>
+
+							</div>
+
+							<div class="tab-pane fade" id="contacto">
+
+								<div class="row">
+									<div class="col-md-12">
+										<div class="form-group">
+											<label for="facebook" class="control-label">Facebook:</label>
+											<input type="text" class="form-control" id="facebok" name="facebook" autocomplete="off">
+										</div>
+									</div>
+								</div>
+								<div class="row">
+									<div class="col-md-12">
+										<div class="form-group">
+											<label for="instagram" class="control-label">Instagram:</label>
+											<input type="text" class="form-control" id="instagram" name="instagram" autocomplete="off">
+										</div>
+									</div>
+								</div>
+
+								<div class="row">
+									<div class="col-md-6">
+										<div class="form-group">
+											<label for="twitter" class="control-label">Twitter:</label>
+											<input type="text" class="form-control" id="twitter" name="twitter" autocomplete="off">
+										</div>
+									</div>
+
+									<div class="col-md-6">
+										<div class="form-group">
+											<label for="tiktok" class="control-label">Tiktok:</label>
+											<input type="text" class="form-control" id="tiktok" name="tiktok" autocomplete="off">
+										</div>
+									</div>
+								</div>
+
+							</div>
+						</div>
+
+						</div>
+						<div class="modal-footer">
+							<button type="button" class="btn btn-secondary waves-effect" data-dismiss="modal">Cerrar</button>
+							<button type="submit" class="btn btn-primary waves-effect waves-light" id="guardar_datos">Guardar</button>
+						</div>
+				</form>
+			</div>
+		</div>
+	</div><!-- /.modal -->
+	<?php
+}
+?>
+
+<!-- /.modal
+
+<div class="row">
 							<div class="col-md-12">
 								<div class="form-group">
 									<label for="nombre" class="control-label">Nombre:</label>
@@ -60,15 +205,4 @@ if (isset($conexion)) {
 							</div>
 						</div>
 
-					</div>
-					<div class="modal-footer">
-						<button type="button" class="btn btn-secondary waves-effect" data-dismiss="modal">Cerrar</button>
-						<button type="submit" class="btn btn-primary waves-effect waves-light" id="guardar_datos">Guardar</button>
-					</div>
-				</form>
-			</div>
-		</div>
-	</div><!-- /.modal -->
-	<?php
-}
-?>
+-->
