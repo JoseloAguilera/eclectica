df0cde855a364ba0dd81d5912fc32927e1237ad3
Yanina Escobar <78667442+LuzEscobar@users.noreply.github.com>
2022-12-23 15:42:31 -0300

    ya muestra lo cargado en tmp_transferencia

--- js/transferencia.js
+++ js/transferencia.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    //$("#resultados").load("../ajax/agregar_tmp_trans.php");
+    $("#resultados").load("../ajax/agregar_tmp_trans.php");
     //$("#f_resultado").load("../ajax/incrementa_factura.php");
    // $("#resultados2").load("../ajax/carga_caja.php");
    // $("#resultados3").load("../ajax/carga_resibido.php");
@@ -109,7 +109,6 @@ $(document).ready(function() {
 }) 
 
  function eliminar(id) {
-    console.log("entro aca");
     var des = localStorage.getItem("destino");
     var or = localStorage.getItem("origen");
     $.ajax({
