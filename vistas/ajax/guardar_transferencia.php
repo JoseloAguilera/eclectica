d27e26dff1e9ba1ef3d2f5cf3d1b5c99c42ae0fd
Yanina Escobar <78667442+LuzEscobar@users.noreply.github.com>
2022-12-24 19:06:22 -0300

    Nuevo Stock

--- vistas/ajax/guardar_transferencia.php
+++ vistas/ajax/guardar_transferencia.php
@@ -33,70 +33,32 @@ if (empty($_POST['or']) || empty($_POST['des'])) {
         $id_tmp          = $row["id_tmp"];
         $id_producto     = $row['id_producto'];
         $cantidad        = $row['cantidad_tmp'];
-
-        //Insert en la tabla detalle_factura
-        $insert_detail = mysqli_query($conexion, "INSERT INTO detalle_fact_ventas VALUES (NULL,'$id_factura','$numero_factura','$id_producto','$cantidad','$desc_tmp','$precio_venta','$precio_total')");
-        //GURDAMOS LAS EN EL KARDEX
-        $saldo_total = $cantidad * $costo_producto;
-        $sql_kardex  = mysqli_query($conexion, "select * from kardex where producto_kardex='" . $id_producto . "' order by id_kardex DESC LIMIT 1");
-        $rww         = mysqli_fetch_array($sql_kardex);
-        $id_producto = $rww['producto_kardex'];
-        $costo_saldo = $rww['costo_saldo'];
-        $cant_saldo  = $rww['cant_saldo'] - $cantidad;
-        //$nueva_cantidad = $cant_saldo - $cantidad;
-        $nuevo_saldo = $cant_saldo * $costo_producto;
-        $tipo        = 2;
-
-        guardar_salidas($date_added, $id_producto, $cantidad, $costo_producto, $saldo_total, $cant_saldo, $costo_saldo, $nuevo_saldo, $date_added, $users, $tipo);
-// FIN
         // ACTUALIZA EN EL STOCK
-        $sql2    = mysqli_query($conexion, "select * from productos where id_producto='" . $id_producto . "'");
+        //Obtiene stock del origen
+        $sql2    = mysqli_query($conexion, "select * from $or where id_producto='" . $id_producto . "'");
         $rw      = mysqli_fetch_array($sql2);
-        $old_qty = $rw['stock_producto']; //Cantidad encontrada en el inventario
-        $new_qty = $old_qty - $cantidad; //Nueva cantidad en el inventario
-        $update  = mysqli_query($conexion, "UPDATE productos SET stock_producto='" . $new_qty . "' WHERE id_producto='" . $id_producto . "' and inv_producto=0"); //Actualizo la nueva cantidad en el inventario
-        $nums++;
+        $old_stock_or = $rw['stock_producto']; //Cantidad encontrada en el inventario
+
+        //obtiene stock del destino
+        $sql3    = mysqli_query($conexion, "select * from $des where id_producto='" . $id_producto . "'");
+        $rw3      = mysqli_fetch_array($sql3);
+        if($rw3){
+            $old_stock_des = $rw3['stock_producto']; //Cantidad encontrada en el inventario
+        }else{
+            $old_stock_des = 0; //Cantidad encontrada en el inventario
+        }
+        $new_stock_or = $old_stock_or - $cantidad; //Nueva cantidad en el inventario
+        $new_stock_des = $old_stock_des + $cantidad; //Nueva cantidad en el inventario
+
+        //$update  = mysqli_query($conexion, "UPDATE productos SET stock_producto='" . $new_qty . "' WHERE id_producto='" . $id_producto . "' and inv_producto=0"); //Actualizo la nueva cantidad en el inventario
+        //$nums++;
     }
     // Fin de la consulta Principal
-    $subtotal         = number_format($sumador_total, 0, '', '');
-    //$total_iva        = ($subtotal * $impuesto) / 100;
-    //$total_iva        = number_format($total_iva, 2, '.', '') - number_format($t_iva, 2, '.', '');
-    $total_factura    = $sumador_total;
-    $cambio           = $resibido - $total_factura;
-    $saldo_credito    = $total_factura - $resibido;
-    $camb             = number_format($cambio, 0, '', '.');
-    $resibido_formato = number_format($resibido, 0, '', '.');
-    if ($condiciones == 4) {
-        $insert_prima = mysqli_query($conexion, "INSERT INTO creditos VALUES (NULL,'$numero_factura','$date_added','$id_cliente','$id_vendedor','$total_factura','$saldo_credito','1','$users','1')");
-        $sql_credito = mysqli_query($conexion, "SELECT MAX(id_credito) as cod FROM creditos");
-        $numero_cred = mysqli_fetch_array($sql_credito);
-        $numero_credito = $numero_cred['cod'];
-        $insert_abono = mysqli_query($conexion, "INSERT INTO creditos_abonos VALUES (NULL,'$numero_factura','$date_added','$id_cliente','$total_factura','$resibido','$saldo_credito','$users','1','CREDITO INICIAL','$numero_credito','0')");
-    }
-    $insert = mysqli_query($conexion, "INSERT INTO facturas_ventas VALUES (NULL,'$numero_factura','$date_added','$id_cliente','$id_vendedor','$condiciones','$total_factura','$estado','$users','$resibido','1','$id_comp','$trans')");
-    $delete = mysqli_query($conexion, "DELETE FROM tmp_ventas WHERE session_id='" . $session_id . "'");
+
+    //$delete = mysqli_query($conexion, "DELETE FROM tmp_ventas WHERE session_id='" . $session_id . "'");
     // SI TODO ESTA CORRECTO
-    if ($condiciones == 5) {
-        echo "<script>
-        swal({
-          title: 'VENTA AL CREDITO GUARDADA CON EXITO CON ANTICIPO DE: $simbolo_moneda $resibido_formato',
-          text: 'Factura: $numero_factura',
-          type: 'success',
-          confirmButtonText: 'ok'
-      })
-  </script>";
-        exit;
-    }
-    if ($insert_detail) {
-        echo "<script>
-        $('#outer_comprobante').load('../ajax/carga_correlativos.php');
-        $('#resultados5').load('../ajax/carga_num_trans.php')
-        $('#modal_vuelto').modal('show');
-        </script>";
-        #$messages[] = "Venta  ha sido Guardada satisfactoriamente.";
-    } else {
-        $errors[] = "Lo siento algo ha salido mal intenta nuevamente." . mysqli_error($conexion);
-    }
+    
+    
 } else {
     $errors[] = "Error desconocido.";
 }
@@ -129,37 +91,3 @@ foreach ($messages as $message) {
 }
 
 ?>
-<!-- Modal -->
-<div class="modal fade" id="modal_vuelto" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                <h4 class="modal-title" id="myModalLabel"><i class='fa fa-edit'></i> FACTURA: <?php echo $numero_factura; ?></h4>
-            </div>
-            <div class="modal-body" align="center">
-            <?php if ($condiciones == 4){ ?>
-                    <strong><h3>VENTA AL CREDITO GUARDADA CON EXITO CON ANTICIPO DE: </h3></strong>
-                    <div class="alert alert-info" align="center">
-                    <strong><h1>
-                        <?php echo $simbolo_moneda . ' ' . $resibido_formato; ?>
-
-                    </h1></strong>
-                    </div>
-                <?php }else{ ?>
-                    <strong><h3>CAMBIO</h3></strong>
-                    <div class="alert alert-info" align="center">
-                    <strong><h1>
-                        <?php echo $simbolo_moneda . ' ' . $camb; ?>
-
-                    </h1></strong>
-                    </div>
-                    <?php } ?>
-            </div>
-            <div class="modal-footer">
-                <!--button type="button" id="imprimir" class="btn btn-primary btn-block btn-lg waves-effect waves-light" onclick="printOrder('1');" accesskey="t" ><span class="fa fa-print"></span> Ticket</button><br-->
-                <button type="button" id="imprimir2" class="btn btn-success btn-block btn-lg waves-effect waves-light" onclick="printFactura('1');" accesskey="p"><span class="fa fa-print"></span> Factura</button>
-            </div>
-        </div>
-    </div>
-</div>
