80b9750a9088f928d4a13d4884ec42c432213860
Yanina Escobar <78667442+LuzEscobar@users.noreply.github.com>
2023-01-01 19:44:59 -0300

    arreglo clientes

--- vistas/modal/registro_cliente.php
+++ vistas/modal/registro_cliente.php
@@ -85,33 +85,6 @@ if (isset($conexion)) {
 								</div>
 
 								<div class="row">
-									<div class="col-md-6">
-										<div class="form-group">
-											<label for="proveedor" class="control-label">Proveedor</label>
-											<select class="form-control" id="proveedor" name="proveedor" required>
-												<option value="">-- Selecciona --</option>
-												<?php
-												$query_proveedor = mysqli_query($conexion, "select * from proveedores order by nombre_proveedor");
-												while ($rw = mysqli_fetch_array($query_proveedor)) {
-												?>
-
-												<option value="<?php echo $rw['id_proveedor']; ?>"><?php echo $rw['nombre_proveedor']; ?></option>
-                                                    <?php
-												}
-													?>
-											</select>
-										</div>
-										
-									</div>
-									<div class="col-md-6">
-										<div class="form-group">
-											<label for="nro_comprobante" class="control-label">Nro Comprobante</label>
-											<input type="text" class="form-control" id="nro_comprobante" name="nro_comprobante" autocomplete="off" required>
-										</div>
-									</div>
-								</div>
-
-								<div class="row">
 									<div class="col-md-3">
 										<label for="hijos" class="control-label">Tiene Hijos</label>
 										<input type="hidden" name="hijos" value="N" />
